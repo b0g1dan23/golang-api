87cedaebc084e7b600da5481ce07ea3084213bef
Bogdan Stevanovic <bogdan.stevanovic090204@gmail.com>
2025-11-22 23:09:15 +0100

    fixed small lint error

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import { checkUserLoggedIn, decodeVerifyAuthToken, refreshAuthToken } from "@/actions/utils";
 
 const poppins = Poppins({
   variable: "--font-poppins",
